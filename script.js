dcc6070f8b8ada0577db40ea384954d3a5180a9d
humberto <contagithubHsds@gmail.com>
2023-12-31 18:47:22 -0300

    add the event, click Enter

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // 8c709e30b79cbf7872f0ba9e29af2650
 
+// apertando o botao
+
  async function SearchPlace() {
     try {
         const carregando = document.querySelector('.loading')
@@ -51,3 +53,62 @@
  } 
 }
 
+// apertando enter
+
+
+
+async function teclaEnter(e) {
+    if (e.key === 'Enter') {
+        try {
+            const carregando = document.querySelector('.loading')
+    
+            carregando.style.display = 'block'
+    
+        const cidade = document.querySelector('.place').value
+    
+        if(cidade == '') {
+            alert('adicione um lugar')
+            carregando.style.display = 'none'
+            return
+        }
+        const city = cidade;
+    
+        apiKey = '8c709e30b79cbf7872f0ba9e29af2650'
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt`)
+    
+        const data = await response.json()
+    
+        document.querySelector('.content .contentPlace h1').innerHTML = `${data.name.toUpperCase()} - ${data.sys.country}`
+    
+        document.querySelector('.content .contentTemp h1').innerHTML = `${data.main.temp.toFixed(0)}°C`
+    
+        document.querySelector('.content .contentHumidit h1').innerHTML = `${data.main.humidity}%`
+    
+        const iconCode = data.weather[0].icon;
+    
+        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`
+    
+        iconImg = document.querySelector('.content .weather-icon #weatherIcon')
+    
+        iconImg.src = iconUrl;
+    
+        document.querySelector('.content').style.display = 'block'
+    
+        carregando.style.display = 'none'
+    
+        document.querySelector('.place').value = ''
+    
+     } catch(error) {
+        console.log('erro na requisicao' + error)
+        alert('Ou voce escreveu errado, ou ocorreu um erro na requisicao')
+        const carregando = document.querySelector('.loading')
+        carregando.style.display = 'none'
+        document.querySelector('.content').style.display = 'none'
+        document.querySelector('.place').value = ''
+        
+     } 
+    }
+
+}
+
+document.addEventListener('keyup', teclaEnter)
